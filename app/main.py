d4a1103d92c72da526b8b64f8abca76e24cd362d
Nkwopara Paachal <paschal0623chizi@gmail.com>
2024-08-17 15:10:16 +0100

    okay

--- app/main.py
+++ app/main.py
@@ -1,17 +1,21 @@
 from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
 from app.auth import auth_router
 
 app = FastAPI()
 
+# Mount the static files directory
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+# Set up templates directory
 templates = Jinja2Templates(directory="templates")
 
+# Include the authentication router
 app.include_router(auth_router)
 
+# Root endpoint
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
     return templates.TemplateResponse("auth.html", {"request": request})
